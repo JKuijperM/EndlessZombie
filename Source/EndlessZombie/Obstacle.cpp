bb9405921f6c8c0f5174d349db486e6509be3413
JKuijperM <jkmontero17@gmail.com>
2023-02-05 18:39:29 +0100

    Added some folders to ignore

---
+++ Source/EndlessZombie/Obstacle.cpp
@@ -0,0 +1,27 @@
+// Copyright Jorge Kuijper. All Rights Reserved.
+
+
+#include "Obstacle.h"
+
+// Sets default values
+AObstacle::AObstacle()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AObstacle::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AObstacle::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
